2f7b337cf89887e080d86b8ba188451560fec5c5
AnjayGoel <angoel123@gmail.com>
2018-12-05 13:48:54 +0530

    Ask user for permissions if not granted

--- Memoir/app/src/main/java/com/sagar/memoir/MainActivity.java
+++ Memoir/app/src/main/java/com/sagar/memoir/MainActivity.java
@@ -1,13 +1,19 @@
 package com.sagar.memoir;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -33,7 +39,31 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(this);
 
+        //Check for permissions
+        if (Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP&&ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // if Permission is not granted
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.READ_EXTERNAL_STORAGE}, 101);
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 101:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //granted
+                    Log.d("pref","Granted");
+                } else {
+                    //not granted
+                    Log.d("pref","Not Granted");
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Memoir/app/src/main/java/com/sagar/memoir/SettingFragment.java
+++ Memoir/app/src/main/java/com/sagar/memoir/SettingFragment.java
@@ -12,6 +12,7 @@ import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.InputFilter;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
